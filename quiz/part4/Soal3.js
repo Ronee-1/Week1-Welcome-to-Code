2c8400c4224b838012ec6893b07ea41e759590b9
muhyusrilirawan2005@gmail.com <muhyusrilirawan2005@gmail.com>
2025-08-15 23:07:13 +0700

    ujian

--- quiz/part4/Soal3.js
+++ quiz/part4/Soal3.js
@@ -5,10 +5,7 @@
 //bikin fungsinya disini
 
 function processSentence(name,age,address,hobby){
-    return `Nama saya ${name}, 
-    umur saya ${age} tahun, 
-    alamat saya di ${address}, 
-    dan saya punya hobby yaitu ${hobby}`;
+    return `Nama saya ${name}, umur saya ${age} tahun, alamat saya di ${address}, dan saya punya hobby yaitu ${hobby}`;
 }
 
 let name = "Agus";
---
+++ quiz/ujian/Soal3.js
@@ -0,0 +1,21 @@
+// Problem
+// Diberikan sebuah function konversiMenit(menit) yang menerima satu parameter berupa angka yang merupakan ukuran waktu 
+// dalam menit. Function akan me-return string waktu dalam format jam:menit berdasarkan menit tersebut. 
+// Contoh, jika menit adalah 63, maka function akan me-return "1:03".
+
+function konversiMenit(menit) {
+    let konversi1 = Math.floor(menit / 60);
+    let konversi2 =  menit % 60;
+    if (konversi2 < 10){
+        return konversi1 + ":0" +konversi2;
+    }else{
+        return konversi1 + ":" + konversi2;
+    }
+}
+  
+// TEST CASES
+console.log(konversiMenit(63)); // 1:03
+console.log(konversiMenit(124)); // 2:04
+console.log(konversiMenit(53)); // 0:53
+console.log(konversiMenit(88)); // 1:28
+console.log(konversiMenit(120)); // 2:00
