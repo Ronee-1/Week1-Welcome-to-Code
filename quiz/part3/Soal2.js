d9a31e26945a900f24645cebffc2faec1764d45e
muhyusrilirawan2005@gmail.com <muhyusrilirawan2005@gmail.com>
2025-08-15 22:26:08 +0700

    part 3 dan 4

---
+++ quiz/part3/Soal2.js
@@ -0,0 +1,17 @@
+// Problem
+// Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript dengan menggunakan syntax for. 
+// Untuk membuat tantangan ini lebih menarik, kamu juga diminta untuk membuat suatu looping yang menghitung maju dan 
+// menghitung mundur. Jangan lupa tampilkan di console juga judul 'LOOPING PERTAMA' dan 'LOOPING KEDUA'.
+//tampilkan output yang menunjukan kalau looping itu maju pada looping pertama dan mundur pada looping kedua
+
+console.log('looping maju')
+for (let maju = 0; maju <= 10; maju++){
+    console.log(maju);
+}
+
+console.log();
+
+console.log('looping mundur');
+for (let mundur = 10; mundur >= 0; mundur--){
+    console.log(mundur);
+}
---
+++ quiz/part4/Soal1.js
@@ -0,0 +1,15 @@
+// Tugas 1
+// Buatlah sebuah fungsi bernama shoutOut(), yang mengembalikan nilai berupa "Halo Function!", 
+// yang kemudian akan ditampilkan di console.
+
+//bikin fungsinya disini
+
+function shoutOut(Halo){
+    return Halo;
+}
+
+console.log(shoutOut('Halo Function'));
+
+// ------------------------------------
+console.log('')
+// -----------------------------------
